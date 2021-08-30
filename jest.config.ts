7c0beda1363060576b69d35f396cbe3770e27f37
Hosein <hosein@localhost.localdomain>
2021-08-30 10:47:18 +0430

    jest configuration in progress

--- jest.config.ts
+++ jest.config.ts
@@ -1,11 +1,16 @@
-export default {
-  testPathIgnorePatterns: ['<rootDir>/next','<rootDir>/node_modules'],
-  setupFilesAfterEnv: ['<rootDir>/setupTests.js'],
-  moduleNameMapper: {
-    '/^.+.(css|less|scss|sass)$/': 'identity-obj-proxy'
-  },
-  transform: {
-    '^.+\\.(js|jsx|ts|tsx)$/': '<rootDir>/node_modules/babel-jest',
-    '/^.+.(css|less|scss|sass)$/': 'identity-obj-proxy',
-  }
-}
+// jest.config.ts
+import type {Config} from '@jest/types';
+
+export default async (): Promise<Config.InitialOptions> => {
+  return {
+    testPathIgnorePatterns: ['<rootDir>/next','<rootDir>/node_modules'],
+    setupFilesAfterEnv: ['<rootDir>/setupTests.js'],
+    moduleNameMapper: {
+      '/^.+.(css|less|scss|sass)$/': 'identity-obj-proxy'
+    },
+    transform: {
+      '^.+\\.(js|jsx|ts|tsx)$/': '<rootDir>/node_modules/babel-jest',
+      '/^.+.(css|less|scss|sass)$/': 'identity-obj-proxy',
+    }
+  };
+};
